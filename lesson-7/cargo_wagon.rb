20c01533a88bc1b4df4d6b8b7d4bc0464950db7b
IvanKOS7 <ivankos429@gmail.com>
2021-11-05 22:17:18 +0700

    correction #1

--- lesson-7/cargo_wagon.rb
+++ lesson-7/cargo_wagon.rb
@@ -4,7 +4,7 @@ require_relative 'wagon'
 
 
 class CargoWagon < Wagon
-  def initialize(wagon_type = :cargo)
+  def initialize(wagon_type = 'cargo')
     @wagon_type = wagon_type
   end
 end
--- lesson-7/passenger_wagon.rb
+++ lesson-7/passenger_wagon.rb
@@ -2,7 +2,7 @@ require_relative 'wagon'
 require_relative 'manufacturer'
 require_relative 'instancecounter'
 class PassengerWagon < Wagon
-  def initialize(wagon_type = :passenger)
+  def initialize(wagon_type = 'passenger')
     @wagon_type = wagon_type
   end
 end
